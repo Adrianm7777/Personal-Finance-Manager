7ebd0d8c7c7fcc587f1e54f352d48f4e6b9e3a13
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-05 17:02:41 +0100

    feature: Defined Income and Expense models and applied migrations.

--- backend/finance_manager/finance/models.py
+++ backend/finance_manager/finance/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Income(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2),
+    source = models.CharField(max_length=255),
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.source}: {self.amount}"
+
+
+class Amount(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2),
+    category = models.CharField(max_length=255),
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.category}: {self.amount}"
