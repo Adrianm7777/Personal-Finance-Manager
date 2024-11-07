3707b35816fd3ff67d9b86ad6dd0b871fa78dfb2
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-07 14:08:12 +0100

    feature: Implemented API views for Income and Expense models using viewsets.

--- backend/finance_manager/finance/views.py
+++ backend/finance_manager/finance/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Expense, Income
+from . serializers import ExpenseSerializer, IncomeSerializer
 
-# Create your views here.
+
+class IncomeViewSet(viewsets.ModelViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
