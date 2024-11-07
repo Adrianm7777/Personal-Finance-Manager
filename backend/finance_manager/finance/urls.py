e363709c36aa9c10be9172845ec38d8139bf3c3f
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-07 14:42:30 +0100

    fixed: typo in urls pattern

--- backend/finance_manager/finance/urls.py
+++ backend/finance_manager/finance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ExpenseViewSet, IncomeViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'incomes', IncomeViewSet)
 router.register(r'expenses', ExpenseViewSet)
 
