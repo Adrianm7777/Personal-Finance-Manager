f45c8834cb86eaa81fd054f72f6c1acd6637de4b
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-18 17:25:16 +0100

    fixed: Add commas to fields in Income and Expense models and perform migrations

--- backend/finance_manager/finance/models.py
+++ backend/finance_manager/finance/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
 class Income(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2),
-    source = models.CharField(max_length=255),
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    source = models.CharField(max_length=255, default=0.00)
     date = models.DateField()
 
     def __str__(self):
         return f"{self.source}: {self.amount}"
 
-
 class Expense(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2),
-    category = models.CharField(max_length=255),
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    category = models.CharField(max_length=255, default=0.00)
     date = models.DateField()
 
     def __str__(self):
