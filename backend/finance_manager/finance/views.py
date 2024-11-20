4cdeb5559fc61b2d1bf393c9fbc8be6c135fc094
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-20 16:38:29 +0100

    feature: integrated automatic categorization into Income and Expense creation

--- backend/finance_manager/finance/views.py
+++ backend/finance_manager/finance/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Expense, Income
-from . serializers import ExpenseSerializer, IncomeSerializer
+from .serializers import ExpenseSerializer, IncomeSerializer
+from .categorization import categorize_transaction
 
 
 class IncomeViewSet(viewsets.ModelViewSet):
     queryset = Income.objects.all()
     serializer_class = IncomeSerializer
 
+    def perform_create(self, serializer):
+        description = self.request.data.get("source", "")
+        category = categorize_transaction(description)
+        serializer.save(category=category)
+
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+
+    def perform_create(self, serializer):
+        description = self.request.data.get("category", "")
+        category = categorize_transaction(description)
+        serializer.save(category=category)
